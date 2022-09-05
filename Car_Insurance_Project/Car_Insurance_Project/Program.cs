94b2ef3066c60045157ae0d5253e2eb42eeb768b
Waderjohnson <104044129+Waderjohnson@users.noreply.github.com>
2022-09-04 18:51:57 -0600

    Add files via upload

---
+++ Car_Insurance_Project/Car_Insurance_Project/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Car_Insurance_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the basic approval program. Find out if you qaulify for car insurance.");
+
+            //Drivers Age
+            Console.WriteLine("What is your age?");
+            string ageStr = Console.ReadLine();
+            int age = Convert.ToInt16(ageStr);
+
+            //Drivers DUI record
+            Console.WriteLine("Have you ever had a DUI? Please answer \"true\" or \"false\".");
+            string duiStr = Console.ReadLine();
+            bool dui = Convert.ToBoolean(duiStr);
+
+            //Drivers Speeding record
+            Console.WriteLine("How many speeding tickets do you have?");
+            string speedStr = Console.ReadLine();
+            int speed = Convert.ToInt16(speedStr);
+
+            //Is driver qualified for insurance
+            bool qualified = (age > 15 && dui == false && speed <= 3);
+            Console.WriteLine("Please see below if you qualify for car insurance:");
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}

969f0b3011acda804bde2256ccb76836e228f46b
Waderjohnson <104044129+Waderjohnson@users.noreply.github.com>
2022-09-07 21:46:57 -0600

    Add files via upload

---
+++ Main_Method/Main_Method/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main_Method
+{
+    class DoMath
+    {
+        //a public method that performs an addition calulation from a value passed in from the method call.  The method returns the value from the addition calculation.
+        public int addition(int num)
+        {
+            int doneMath = num + 111;
+            return doneMath;
+        }
+        //a overflow method that performs a subtraction calulation from a value passed in from the method call.  The method returns the value from the subtraction calculation.
+        public decimal addition(decimal dec)
+        {
+            decimal doneMath = dec - .315m;
+            return doneMath;
+        }
+        //a overflow method that performs an multiplication calulation from a value passed in from the method call.  The value passed in as a string is converted within the method
+        //to an int and returns an int value. The method returns the value from the multiplication calculation.
+        public int addition(string aString)
+        {
+            int number = Convert.ToInt32(aString);
+            int doneMath = number * 35;
+            return doneMath;
+        }
+    }
+}
---
+++ Main_Method/Main_Method/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Main_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating the Class DoMath
+            DoMath doMath = new DoMath();
+
+            //Calling the mathod and its overflows from the class in a WriteLint method that will display the returned result of the what the method code returns.  In this case the answers to 
+            //addition, subtraction and multiplication calculations.  Each method passing in a different value.  An int for the first called method, a decimal for the second and a string
+            //for the third.  The string is converted to an int and returned as an int value.
+            Console.WriteLine("111 + 3 = " + doMath.addition(3));
+            Console.WriteLine(".66 - .315 = " + doMath.addition(.66m));
+            Console.WriteLine("65 * 35 = " + doMath.addition("65"));
+            Console.ReadLine();
+        }
+    }
+    }

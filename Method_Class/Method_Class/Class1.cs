bd306fbd71ce992f02585cdc1529933908e0155d
Waderjohnson <104044129+Waderjohnson@users.noreply.github.com>
2022-09-08 15:22:14 -0600

    Method Class assignment

---
+++ Method_Class/Method_Class/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Class
+{
+    public class Math
+    {
+        //a public void method that does not return a value.  The results anything done and or displayed in from the method itself.  An addition calculation is done to num1 and saved to the 
+        //variable mathdone.  num2 is printed to the console using the WriteLive method.  The method is being called from the main() Program.cs
+        public void addition(int num1, int num2)
+        {
+            int mathdone = num1 + 55;
+            Console.WriteLine(num2);
+        }
+
+    }
+}
---
+++ Method_Class/Method_Class/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the class Math
+            Math math = new Math();
+
+            //a call to the method in class math with int values being passed as the parameters.
+            math.addition(45, 92);
+            Console.ReadLine();
+            //a call to the method in class math with the parameters set by name with thier values.
+            math.addition(num1: 25, num2: 56);
+            Console.ReadLine();
+        }
+    }
+}

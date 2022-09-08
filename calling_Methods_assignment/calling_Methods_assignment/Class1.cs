98844a82c1deaff169fbf95d646803267936be38
Waderjohnson <104044129+Waderjohnson@users.noreply.github.com>
2022-09-08 15:07:31 -0600

    updated to fulfill requirements of assignment

--- calling_Methods_assignment/calling_Methods_assignment/Class1.cs
+++ calling_Methods_assignment/calling_Methods_assignment/Class1.cs
@@ -1,56 +1,33 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace calling_Methods_assignment
+namespace Methods
 {
-    public class MathClass
+    public class Methods
     {
-        //the class parameter value from the input variable inputnumber.
-        public int inputnumber { get; set; }
-
-        //a method for doing subtraction with the value of the parameter input number applied to the variable num. num - 55 = value saved to the variable subtraction. With a console writeline 
-        //to display a statement showing the operation with the value of num and subtraction concatenated into the statement.
-        //an if statement applied to avoid a negative number from subtracting a smaller number than 55
-        public void subMath(int num)
+        //a method for doing addition with the value of the parameter input number applied to the variable num. num + 55 = value saved to the variable addition. With a return to return value to calling method.
+        public static int addInt(int num)
         {
-            if (num >= 55)
-            {
-                int subtraction = num - 55;
-                Console.WriteLine("Subtraction: " + num + " - 55 = " + subtraction);
-            }
-            else
-            {
-                int subtraction = 55 - num;
-                Console.WriteLine("Subtraction: 55 - " + num + " = " + subtraction);
-            }
+            int myNum = num;
+            int myNumAdd = myNum + 55;
+            return myNumAdd;
+        }
+        //a method for doing subtraction with the value of the parameter input number applied to the variable num. num - 55 = value saved to the variable subtraction. With a return to return value to calling method.
+
+        public static int subInt(int num)
+        {
+            int myNum = num;
+            int myNumSub = myNum - 55;
+            return myNumSub;
         }
 
-        //a method for doing addition with the value of the parameter input number applied to the variable num. num + 55 = value saved to the variable addition. With a console writeline 
-        //to display a statement showing the operation with the value of num and addition concatenated into the statement.
-        public void addMath(int num)
+        //a method for doing multiplication with the value of the parameter input number applied to the variable num. num * 55 = value saved to the variable multiplication. With a return to return value to calling method.
+        public static int multInt(int num)
         {
-            int addition = num + 55;
-            Console.WriteLine("Addition: " + num + " + 55 = " + addition);
-        }
-
-        //a method for doing multiplication with the value of the parameter input number applied to the variable num. num * 55 = value saved to the variable multiplication. With a console writeline 
-        //to display a statement showing the operation with the value of num and multiplication concatenated into the statement.
-        public void multiMath(int num)
-        {
-            int multiplication = num * 55;
-            Console.WriteLine("Multiplication: " + num + " * 55 = " + multiplication);
-        }
-
-        //a method for finding the square root with the value of the parameter input number applied to the variable num. utilizing the system Math class with the method Sqrt of the variable sum
-        //= value saved to the variable sqrt. This method makes a return value to the method that will used in a Writeline methode statement (mathClass.sqrtMath(mathClass.inputnumber) concatinated
-        //into the statement )
-        public double sqrtMath(int num)
-        {
-            double sqrt = Math.Sqrt(num);
-            return sqrt;
+            int myNum = num;
+            int myNumMult = myNum * 55;
+            return myNumMult;
         }
     }
 }

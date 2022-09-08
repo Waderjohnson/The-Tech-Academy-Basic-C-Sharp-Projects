2fc2731cc49fb5b2c79ced9f680983466564bd44
Waderjohnson <104044129+Waderjohnson@users.noreply.github.com>
2022-09-07 21:42:22 -0600

    Add files via upload

---
+++ calling_Methods_assignment/calling_Methods_assignment/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace calling_Methods_assignment
+{
+    public class MathClass
+    {
+        //the class parameter value from the input variable inputnumber.
+        public int inputnumber { get; set; }
+
+        //a method for doing subtraction with the value of the parameter input number applied to the variable num. num - 55 = value saved to the variable subtraction. With a console writeline 
+        //to display a statement showing the operation with the value of num and subtraction concatenated into the statement.
+        //an if statement applied to avoid a negative number from subtracting a smaller number than 55
+        public void subMath(int num)
+        {
+            if (num >= 55)
+            {
+                int subtraction = num - 55;
+                Console.WriteLine("Subtraction: " + num + " - 55 = " + subtraction);
+            }
+            else
+            {
+                int subtraction = 55 - num;
+                Console.WriteLine("Subtraction: 55 - " + num + " = " + subtraction);
+            }
+        }
+
+        //a method for doing addition with the value of the parameter input number applied to the variable num. num + 55 = value saved to the variable addition. With a console writeline 
+        //to display a statement showing the operation with the value of num and addition concatenated into the statement.
+        public void addMath(int num)
+        {
+            int addition = num + 55;
+            Console.WriteLine("Addition: " + num + " + 55 = " + addition);
+        }
+
+        //a method for doing multiplication with the value of the parameter input number applied to the variable num. num * 55 = value saved to the variable multiplication. With a console writeline 
+        //to display a statement showing the operation with the value of num and multiplication concatenated into the statement.
+        public void multiMath(int num)
+        {
+            int multiplication = num * 55;
+            Console.WriteLine("Multiplication: " + num + " * 55 = " + multiplication);
+        }
+
+        //a method for finding the square root with the value of the parameter input number applied to the variable num. utilizing the system Math class with the method Sqrt of the variable sum
+        //= value saved to the variable sqrt. This method makes a return value to the method that will used in a Writeline methode statement (mathClass.sqrtMath(mathClass.inputnumber) concatinated
+        //into the statement )
+        public double sqrtMath(int num)
+        {
+            double sqrt = Math.Sqrt(num);
+            return sqrt;
+        }
+    }
+}
---
+++ calling_Methods_assignment/calling_Methods_assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calling_Methods_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Created a class called mathClass to contain methods for math computation.
+            MathClass mathClass = new MathClass();
+
+            //a Writeline method printing to the console a question for the user to make a input.  The input being saved to the variable inputnumber.  The variable inputnumber is also applied to
+            //public int inputnumber { get; set; } in the MathClass as a global paramater for all the methods in the class.
+            Console.WriteLine("Please give a number to be used in three mathematical calculations against the number 55 with a bonus figure\n of the square root of the number you choose.");
+            mathClass.inputnumber = Convert.ToInt32(Console.ReadLine());
+
+            //calls for the methods subMath, addMath, multiMath.
+            mathClass.subMath(mathClass.inputnumber);
+            mathClass.addMath(mathClass.inputnumber);
+            mathClass.multiMath(mathClass.inputnumber);
+
+            //A WriteLine method printing to the console a statement containing the class parameter and the return value from the method sqrtMath concatinated into the statement.
+            Console.WriteLine("The Square root of " + mathClass.inputnumber + " is " + mathClass.sqrtMath(mathClass.inputnumber));
+
+
+            Console.ReadLine();
+        }
+    }
+}

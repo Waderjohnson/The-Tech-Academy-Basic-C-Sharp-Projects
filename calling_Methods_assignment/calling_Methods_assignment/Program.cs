80a8ae81588468d0e4687013b33cab78517117cf
Waderjohnson <104044129+Waderjohnson@users.noreply.github.com>
2022-09-08 15:08:12 -0600

    updated to fulfill requirements of the assignment

--- calling_Methods_assignment/calling_Methods_assignment/Program.cs
+++ calling_Methods_assignment/calling_Methods_assignment/Program.cs
@@ -1,33 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace calling_Methods_assignment
+
+namespace Methods
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Created a class called mathClass to contain methods for math computation.
-            MathClass mathClass = new MathClass();
+            Console.WriteLine("Enter an integer to perform basic math operations on.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
 
-            //a Writeline method printing to the console a question for the user to make a input.  The input being saved to the variable inputnumber.  The variable inputnumber is also applied to
-            //public int inputnumber { get; set; } in the MathClass as a global paramater for all the methods in the class.
-            Console.WriteLine("Please give a number to be used in three mathematical calculations against the number 55 with a bonus figure\n of the square root of the number you choose.");
-            mathClass.inputnumber = Convert.ToInt32(Console.ReadLine());
+            int newNumAdd = Methods.addInt(userNum);
+            int newNumSub = Methods.subInt(userNum);
+            int newNumMult = Methods.multInt(userNum);
 
-            //calls for the methods subMath, addMath, multiMath.
-            mathClass.subMath(mathClass.inputnumber);
-            mathClass.addMath(mathClass.inputnumber);
-            mathClass.multiMath(mathClass.inputnumber);
-
-            //A WriteLine method printing to the console a statement containing the class parameter and the return value from the method sqrtMath concatinated into the statement.
-            Console.WriteLine("The Square root of " + mathClass.inputnumber + " is " + mathClass.sqrtMath(mathClass.inputnumber));
-
-
+            Console.WriteLine("Your number plus 55 is: " + newNumAdd);
+            Console.WriteLine("Your number subtracted by 55 is: " + newNumSub);
+            Console.WriteLine("Your number multiplied by 55 is: " + newNumMult);
             Console.ReadLine();
         }
     }
-}
+    }

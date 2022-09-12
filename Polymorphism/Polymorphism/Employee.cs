fa49104df14ae6a948eb87bbfd5dcf74e44fb73d
Waderjohnson <104044129+Waderjohnson@users.noreply.github.com>
2022-09-11 18:40:36 -0600

    Add files via upload

---
+++ Polymorphism/Polymorphism/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Employee name is: {0} {1}", FirstName, LastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("This employee has quit");
+            FirstName = "";
+            LastName = "";
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person1 = new Employee();
+            person1.FirstName = "Sample";
+            person1.LastName = "Student";
+            person1.SayName();
+
+            //Use polymorphism to create an object of type IQuittable and call the Quit() method on it.
+            person1.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}

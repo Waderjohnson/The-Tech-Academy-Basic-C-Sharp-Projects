e42422ad6f79c523a02bf318459eb9327548b1a0
Waderjohnson <104044129+Waderjohnson@users.noreply.github.com>
2022-09-11 18:28:32 -0600

    Add files via upload

---
+++ AbstractClass/AbstractClass/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    //inherit from the Person class
+    public class Employee : Person
+    {
+        ////Implement the SayName() method inside of the Employee class
+        public override void sayName()
+        {
+            Console.WriteLine("Employee name is: " + firstName + " " + lastName);
+        }
+
+    }
+}
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate an Employee object
+            Employee employee1 = new Employee();
+
+            employee1.firstName = "Sample";
+            employee1.lastName = "Student";
+            employee1.sayName();
+
+            Console.ReadLine();
+        }
+    }
+}

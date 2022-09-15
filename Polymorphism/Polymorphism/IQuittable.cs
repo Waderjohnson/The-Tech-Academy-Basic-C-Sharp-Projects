1cef3d2a184ab7fb0bf3b2d3bf92729ced1a0cfd
Waderjohnson <104044129+Waderjohnson@users.noreply.github.com>
2022-09-14 18:32:32 -0600

    Update IQuittable.cs

--- Polymorphism/Polymorphism/IQuittable.cs
+++ Polymorphism/Polymorphism/IQuittable.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +9,6 @@ namespace Polymorphism
     interface IQuittable
     {
         void Quit();
-
     }
+
 }
